a7896a73b7b5ea6a805e4862efb2ce8fa6641407
kejieleung <kejieleung@gmail.com>
2025-12-11 13:29:06 +0800

    fix: Enable typography plugin for markdown formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('@tailwindcss/typography'),
+  ],
 }
